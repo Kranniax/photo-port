2035405349af89e1a3e2d0e8594134d5a735f961
Shane Wade <shanebramble96@hotmail.com>
2026-02-02 19:46:25 -0600

    Initialize React Application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import About from "./components/About/index.js";
+import About from "./components/About/index.jsx";
+import Nav from "./components/Nav/index.jsx";
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
