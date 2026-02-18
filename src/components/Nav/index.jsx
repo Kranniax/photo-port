86b4b08255345e71ff92e836fec86d834166cb37
Shane Wade <shanebramble96@hotmail.com>
2026-02-17 18:44:31 -0600

    Add useEffect

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,22 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers.js";
 
-function Nav() {
-  const [categories] = useState([
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-  ]);
+function Nav(props) {
+  const { categories = [], setCurrentCategory, currentCategory } = props;
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header>
