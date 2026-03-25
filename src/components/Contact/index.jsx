8d6a4c91962c24117880887a0b73b598d7ee7a02
Shane Wade <shanebramble96@hotmail.com>
2026-03-24 19:38:35 -0500

    Create SPA for contact form and nav categories

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -10,15 +10,28 @@ function ContactForm() {
   });
   const { name, email, message } = formState;
   const [errorMessage, setErrorMessage] = useState("");
-  
+
   function handleChange(e) {
     if (e.target.name === "email") {
       const isValid = validateEmail(e.target.value);
       console.log(isValid);
 
       // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("You email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
     }
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
   }
   function handleSubmit(e) {
     e.preventDefault();
@@ -33,7 +46,7 @@ function ContactForm() {
           <input
             type="text"
             defaultValue={name}
-            onChange={handleChange}
+            onBlur={handleChange}
             name="name"
           />
         </div>
@@ -43,7 +56,7 @@ function ContactForm() {
             type="email"
             name="email"
             defaultValue={email}
-            onChange={handleChange}
+            onBlur={handleChange}
           />
         </div>
         <div>
@@ -52,9 +65,14 @@ function ContactForm() {
             name="message"
             rows="5"
             defaultValue={message}
-            onChange={handleChange}
+            onBlur={handleChange}
           />
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">Submit</button>
       </form>
     </section>
